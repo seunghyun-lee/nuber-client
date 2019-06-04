39da6c915b19e2a21c2e4bed673852fbb2f17afc
seunghyun <seunghyunleekr@gmail.com>
2019-06-04 17:56:46 +0900

    updating driver mode

--- src/Components/Menu/MenuPresenter.tsx
+++ src/Components/Menu/MenuPresenter.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { MutationFn } from "react-apollo";
 import { Link } from "react-router-dom";
 import styled from "../../typed-components";
-import { userProfile, userProfile_GetMyProfile } from "../../types/api";
+import { toggleDriving, userProfile } from "../../types/api";
+
 
 const Container = styled.div`
   height: 100%;
@@ -78,45 +80,43 @@ const ToggleDriving = styled<IToggleProps, any>("button")`
 interface IProps {
   data?: userProfile;
   loading: boolean;
+  toggleDrivingFn: MutationFn<toggleDriving>;
 }
 
-const MenuPresenter: React.SFC<IProps> = (data, loading) => {
-    const GetMyProfile = data.data;
-    if (GetMyProfile) {
-      const response: userProfile_GetMyProfile = GetMyProfile.GetMyProfile;
-      if (response && response.ok && response.user) {
-        const user = response.user;
-        return (
-          <Container>
-            <Header>
-              <Grid>
-                <Link to={"/edit-account"}>
-                  <Image
-                    src={
-                      user.profilePhoto ||
-                      "https://yt3.ggpht.com/-CTwXMuZRaWw/AAAAAAAAAAI/AAAAAAAAAAA/HTJy-KJ4F2c/s88-c-k-no-mo-rj-c0xffffff/photo.jpg"
-                    }
-                  />
-                </Link>
-                <Text>
-                  <Name>{user.fullName}</Name>
-                  <Rating>4.5</Rating>
-                </Text>
-              </Grid>
-            </Header>
-            <SLink to="/trips">Your Trips</SLink>
-            <SLink to="/settings">Settings</SLink>
-            <ToggleDriving isDriving={user.isDriving}>
-              {user.isDriving ? "Stop driving" : "Start driving"}
-            </ToggleDriving>
-          </Container>
-        );
-      } else {
-        return <div>can't load</div>;
-      }
-    } else {
-      return <div>can't load</div>;
-    }
+const MenuPresenter: React.SFC<IProps> = ({
+  data: { GetMyProfile: { user = null } = {} } = null, 
+  loading,
+  toggleDrivingFn
+}) => {
+    <Container>
+      {!loading &&
+      user &&
+      userInfo.fullName && (
+        <React.Fragment>
+          <Header>
+            <Grid>
+              <Link to={"/edit-account"}>
+                <Image
+                  src={
+                    user.profilePhoto ||
+                    "https://yt3.ggpht.com/-CTwXMuZRaWw/AAAAAAAAAAI/AAAAAAAAAAA/HTJy-KJ4F2c/s88-c-k-no-mo-rj-c0xffffff/photo.jpg"
+                  }
+                />
+              </Link>
+              <Text>
+                <Name>{user.fullName}</Name>
+                <Rating>4.5</Rating>
+              </Text>
+            </Grid>
+          </Header>
+          <SLink to="/trips">Your Trips</SLink>
+          <SLink to="/settings">Settings</SLink>            
+          <ToggleDriving onClick={toggleDrivingFn} isDriving={user.isDriving}>
+            {user.isDriving ? "Stop driving" : "Start driving"}
+          </ToggleDriving>
+        </React.Fragment>
+      )}
+    </Container>    
 };
 
 export default MenuPresenter;
