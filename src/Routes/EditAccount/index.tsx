0bf72d625643f3aac2ecca983522b50c9219d110
seunghyun <seunghyunleekr@gmail.com>
2019-06-25 17:51:11 +0900

    add editaccount

--- src/Routes/EditAccount/index.tsx
+++ src/Routes/EditAccount/index.tsx
@@ -1,5 +1,2 @@
-import React from "react";
-
-const R = () => <span>haha</span>;
-
-export default R;
+import EditAccountContainer from "./EditAccountContainer";
+export default EditAccountContainer;
